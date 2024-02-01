5815d4171b26c80ff7b5345d5dd464e37366f9e8
snipes <schneidsmc@gmail.com>
2024-01-31 22:02:11 -0600

    README is generating using prompts from  inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -6,13 +6,62 @@ const generateReadme = require("./utils/generateMarkdown.js")
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptModule(){
+return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Describe your project. Why was it built? What problem does it solve?',
+            name: 'description'
+        },
+        {
+            type: 'checkbox',
+            message: 'What licenses are utilized?',
+            choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'GNU', 'Mozilla', 'Open'],
+            name: 'license'
+        },
+        {
+            type: 'checkbox',
+            message: 'Check the technologies, languages or systems used to create this.',
+            choices: ['HTML', 'CSS', 'Markdown', 'Javascript', 'JQuery', 'NodeJS'],
+            name: 'stack',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions for the usage of this project.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions for the installation of this project.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub Username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+    ])};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function init() {
+   try{
+        const data = await promptModule();
+        const generateFile = generateReadme(data);
+fs.promises.writeFile('genREADME.md', generateFile);
+console.log('Sucess! Your professional README has been created!')
+   } catch(err) {
+    console.log(err)
+   }
+} 
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+<h1 align="center"> ${data.title} </h1>
+
+![badge](https://img.shields.io/badge/license-${data.license}-brightblue)<br />
+
+## Description
+
+- What was your motivation? Why did you build this project? What problem does it solve? What did you learn?
+
+${data.description}
+
+This was created using ${data.stack}.
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Author](#author)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+${data.installation}
+
+## Usage
+
+Provide instructions and examples for use.
+
+${data.usage}
+
+## Author
+
+GitHub Username: ${data.username}
+
+Email: ${data.email}
+
 
 `;
 }
